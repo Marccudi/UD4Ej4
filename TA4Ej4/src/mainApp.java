43259228ee7ae1febea10c8a61b62b3246a307f4
Marccudi <marccudi@hotmail.es>
2022-07-06 22:08:25 +0200

    Ejercicio 4 acabado

---
+++ TA4Ej4/src/mainApp.java
@@ -0,0 +1,16 @@
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =100;
+		System.out.println("Valor inicial de N es "+n);
+		n+=77;
+		System.out.println("N + 77 = "+n);
+		n-=3;
+		System.out.println("N - 3 = "+n);
+		n*=2;
+		System.out.println("N * 2 = "+n);
+	}
+
+}
